30e4083d466bb01d6cd15329ae111e3a1b00079e
yiqinghuang930 <weeqqq@qq.com>
2019-04-12 20:04:48 +0800

    Add Animation study code

--- app/src/main/java/com/bai/psychedelic/animationdemo/MainActivity.java
+++ app/src/main/java/com/bai/psychedelic/animationdemo/MainActivity.java
@@ -2,13 +2,64 @@ package com.bai.psychedelic.animationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private Button mScaleAnimation,mAlphaAnimation,mRotateAnimation,mTranslateAnimation;
+    private ImageView mImageView;
+    private Animation animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        mScaleAnimation = findViewById(R.id.main_btn_scale);
+        mAlphaAnimation = findViewById(R.id.main_btn_alpha);
+        mRotateAnimation = findViewById(R.id.main_btn_rotate);
+        mTranslateAnimation = findViewById(R.id.main_btn_move);
+        mImageView = findViewById(R.id.image_view);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_btn_scale:
+                animation = AnimationUtils.loadAnimation(this,R.anim.scale);
+                animation.setFillAfter(false);
+                animation.setFillBefore(true);
+                mImageView.startAnimation(animation);
+
+                break;
+            case R.id.main_btn_alpha:
+                animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+                mImageView.startAnimation(animation);
+                Log.d(TAG, "onClick: main_btn_alpha startAnimation");
+                break;
+            case R.id.main_btn_rotate:
+                animation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+                mImageView.startAnimation(animation);
+                break;
+            case R.id.main_btn_move:
+//                TranslateAnimation translateAnimation = new TranslateAnimation(-50,-50,50,50);
+//                translateAnimation.setDuration(1000);
+//                translateAnimation.setRepeatCount(Animation.INFINITE);
+//                translateAnimation.setRepeatMode(Animation.RESTART);
+//                mImageView.startAnimation(translateAnimation);
+                animation = AnimationUtils.loadAnimation(this,R.anim.translate);
+                animation.setFillBefore(false);
+                animation.setFillAfter(true);
+                mImageView.startAnimation(animation);
+                break;
+        }
+        if (animation!=null){
+        }
     }
 }
